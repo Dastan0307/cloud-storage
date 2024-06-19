61cf1f8ab07e0d4c432cc02906fc76f7198c7ce5
Dastan <nurgazyuuludastan@gmail.com>
2024-06-20 02:37:59 +0600

    init project

---
+++ backend/src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { cors: false });
+
+  app.enableCors({ credentials: false, origin: true });
+
+  const config = new DocumentBuilder()
+    .setTitle('Облачное хранилище')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
+  await app.listen(7777);
+
+}
+bootstrap();
